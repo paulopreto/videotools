d8f2ff5ff5d3d649fcd12d78b8c26d5cfc7a38b5
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2021-07-04 19:50:08 -0300

    update - exit

--- concatvideo.sh
+++ concatvideo.sh
@@ -11,3 +11,24 @@ fi
 ffmpeg -f concat -i "${DIRNAME}/listvideo.txt" -c copy "${DIRNAME}/${DIRNAME}_concat".mp4 -loglevel quiet -nostats -hide_banner
 
 echo "-----------> !!!!!! Done !!!!!"
+
+
+DIRNAME=$1
+if [ ! -d $1 ]; then
+	echo "Folder $1 does not exist!!!" && exit
+fi
+
+if [ $# -gt 1 ]; then
+	if [ $2 -eq 1 ]; then
+		ffmpeg -f concat -i "${DIRNAME}/listvideo.txt" -vcodec libx265 -pix_fmt yuv420p -vtag hvc1 "${DIRNAME}/${DIRNAME}_concat".mp4 -loglevel quiet -nostats -hide_banner
+		echo -e "\nOs arquivos foram juntados e codificado em H265"
+	else
+		ffmpeg -f concat -i "${DIRNAME}/listvideo.txt" -c copy "${DIRNAME}/${DIRNAME}_concat".mp4 -loglevel quiet -nostats -hide_banner
+		echo -e "\n!!!!!! DONE !!!!!"
+	fi	
+else
+	ffmpeg -f concat -i "${DIRNAME}/listvideo.txt" -c copy "${DIRNAME}/${DIRNAME}_concat".mp4 -loglevel quiet -nostats -hide_banner
+	echo -e "\n!!!!!! DONE !!!!!"
+fi
+
+
