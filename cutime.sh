0fc94bfc8bb0dc5809da8b15e347b3ba18775ad8
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2021-07-04 19:43:06 -0300

    compress x265

--- cutime.sh
+++ cutime.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Example to run -> ./cutime.sh videoname.mp4 time_start time_end
 # ./cutime.sh videoname.mp4 00:00:17 00:00:56
-
+#
 # CUTIME: v1.0 - Paulo Santiago 02-07-2021
 
 FILENAME="$1"
 TIMES=$2
 TIMEE=$3
 DIRNAME=`echo "$FILENAME" | cut -d'.' -f1`
-COMPLEMENTO="_cutime"
+COMPLEMENTO="_cut"
 
 if [ ! -d "${DIRNAME}${COMPLEMENTO}" ]; then
 	mkdir "${DIRNAME}${COMPLEMENTO}"
@@ -16,8 +16,19 @@ fi
 
 TS=`echo "$TIMES" | tr : -`
 TE=`echo "$TIMEE" | tr : -`
-ffmpeg -i $FILENAME -ss "${TIMES}" -to "${TIMEE}" -c copy "${DIRNAME}${COMPLEMENTO}"/"${DIRNAME}_${TS}_${TE}".mp4 -loglevel quiet -nostats -hide_banner
+
+
+if [ $# -gt 3 ]; then
+	if [ $4 -eq 1 ]; then
+		#ffmpeg -i $FILENAME -ss "${TIMES}" -to "${TIMEE}" -vcodec libx264 -pix_fmt yuv420p -acodec aac "${DIRNAME}${COMPLEMENTO}"/"${DIRNAME}_${TS}_${TE}".mp4 -loglevel quiet -nostats -hide_banner
+		ffmpeg -i $FILENAME -ss "${TIMES}" -to "${TIMEE}" -vcodec libx265 -pix_fmt yuv420p -vtag hvc1 "${DIRNAME}${COMPLEMENTO}"/"${DIRNAME}_${TS}_${TE}".mp4 -loglevel quiet -nostats -hide_banner
+		echo -e "\nO arquivo além de cortado foi codificado em H265"
+	else
+		ffmpeg -ss "${TIMES}" -i $FILENAME -t "${TIMEE}" -vcodec copy -acodec copy "${DIRNAME}${COMPLEMENTO}"/"${DIRNAME}_${TS}_${TE}".mp4 -loglevel quiet -nostats -hide_banner
+	fi
+else
+	ffmpeg -ss "${TIMES}" -i $FILENAME -t "${TIMEE}" -vcodec copy -acodec copy "${DIRNAME}${COMPLEMENTO}"/"${DIRNAME}_${TS}_${TE}".mp4 -loglevel quiet -nostats -hide_banner
+fi
 
 echo "file '${DIRNAME}_${TS}_${TE}.mp4'" >> "${DIRNAME}${COMPLEMENTO}/listvideo.txt"
-echo "------------> !!!!!! Done !!!!!"
-
+echo -e "\n!!!!!! DONE !!!!!"
