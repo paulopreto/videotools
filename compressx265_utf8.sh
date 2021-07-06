830813cda71207d6e0f0a85661cb9e89b81a0283
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2021-07-06 17:02:52 -0300

    normalize name

---
+++ compressx265_utf8.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Example to run -> ./compressx265.sh
+#
+# COMPRESSX265: v1.0 - Paulo Santiago 04-07-2021
+
+if [ -z $1 ]; then
+  EXTNAME=$"mp4"
+else
+  EXTNAME=$1
+fi
+
+for i in *."${EXTNAME}"; do
+  name=`echo "$i" | cut -d'.' -f1`
+  name=`echo "$name" | iconv -t 'ascii//TRANSLIT'`
+  name=`echo "$name" | sed "s, ,_,g"` 
+  echo "$name"
+  mkdir compressed_x265
+  ffmpeg -i "$i" -vcodec libx265 -pix_fmt yuv420p -vtag hvc1 "./compressed_x265/${name}"_x265".mp4" -loglevel quiet -nostats -hide_banner
+  echo "$name ------------> !!!!!! Done !!!!!"
+done
+
